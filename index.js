6bde7daf5211584a555bec264a5e548c256d4bb0
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-03-12 11:44:11 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 9;
+let num4 = 5;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(3, 5, 19, 20, 4, 7, 13, 15);
